26ff9b3ed7827d17ba60fa0054a528aedc3395f9
ptp28 <parthparikh28@gmail.com>
2020-02-24 11:58:53 +0530

    Added GPIO extension

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -24,6 +24,7 @@ const builtinExtensions = {
     makeymakey: () => require('../extensions/scratch3_makeymakey'),
     boost: () => require('../extensions/scratch3_boost'),
     gdxfor: () => require('../extensions/scratch3_gdx_for')
+    onegpioRpi: () => require('../extensions/scratch3_onegpioRpi')
 };
 
 /**
